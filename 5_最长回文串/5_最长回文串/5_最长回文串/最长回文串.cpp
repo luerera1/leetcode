ddf71b5dff63c5b5313f495d3ddeae1c3a52c725
luerera1 <luerera@163.com>
2020-02-24 22:18:40 +0800

    最长字符串

---
+++ 5_最长回文串/5_最长回文串/5_最长回文串/最长回文串.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+	string longestPalindrome(string s) {
+		if (s.empty())
+			return "";
+		int n = s.size(), dp[n][n] = { 0 }, left = 0, len = 1;
+		for (int i = 0; i<n; i++){
+			dp[i][i] = 1;
+			for (int j = 0; j<i; j++){
+				dp[j][i] = (s[i] == s[j] && (i - j < 2 || dp[j + 1][i - 1]));
+				if (dp[j][i] && len < i - j + 1){
+					len = i - j + 1;
+					left = j;
+				}
+			}
+		}
+		return s.substr(left, len);
+	}
+};
