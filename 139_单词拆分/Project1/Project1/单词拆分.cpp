38fe4d13269e40e6172c7ad43c4cb30850934b30
luerera1 <luerera@163.com>
2020-02-27 14:31:26 +0800

    单词拆分

---
+++ 139_单词拆分/Project1/Project1/单词拆分.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	bool wordBreak(string s, vector<string>& wordDict) {
+		unordered_set<string> word(wordDict.begin(), wordDict.end());
+		vector<bool> dp(s.size() + 1);
+		dp[0] = true;
+		for (int i = 0; i<dp.size(); i++){
+			for (int j = 0; j<i; j++){
+				if (dp[j] && word.count(s.substr(j, i - j))){
+					dp[i] = true;
+					break;
+				}
+			}
+		}
+		return dp.back();
+	}
+};
