3d7c9dd974a584f698454718eb2845e611dbc2b3
luerera1 <luerera@163.com>
2020-05-18 15:38:26 +0800

    循环链表

---
+++ 457_环形链表循环/环形链表循环/环形链表循环/源.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+	bool circularArrayLoop(vector<int>& nums) {
+		int n = nums.size();
+		vector<bool> visited(n);
+		for (int i = 0; i < n; ++i) {
+			if (visited[i]) continue;
+			visited[i] = true;
+			unordered_map<int, int> m;
+			int cur = i;
+			while (true) {
+				int next = ((cur + nums[cur]) % n + n) % n;
+				if (next == cur || nums[next] * nums[cur] < 0) break;
+				if (m.count(next)) return true;
+				m[cur] = next;
+				cur = next;
+				visited[next] = true;
+			}
+		}
+
+		return false;
+	}
+};
