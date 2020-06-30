30920064f4e8fdb318f952259ead6e21917b2d32
luerera1 <luerera@163.com>
2020-06-30 16:51:04 +0800

    0

---
+++ 剑指offer/03/Project1/Project1/源.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	int findRepeatNumber(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		for (int i = 1; i<nums.size(); i++){
+			if (nums[i] == nums[i - 1])
+				return nums[i];
+			else
+				continue;
+		}
+		return -1;
+	}
+};
