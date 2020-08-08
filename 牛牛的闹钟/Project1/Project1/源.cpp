8226664c94667cb11d7b8db1cc834c3ed454757e
luerera1 <luerera@163.com>
2020-08-08 14:41:59 +0800

    -

---
+++ 牛牛的闹钟/Project1/Project1/源.cpp
@@ -0,0 +1,37 @@
+#includeiostream
+#includealgorithm
+using namespace std;
+struct node{
+	int a;
+	int b;
+}a[110], temp, res;
+int cmp(node a, node b){
+	if (a.a == b.a)
+		return a.bb.b;
+	else
+		return a.ab.a;
+}
+int main(){
+	int n;
+	cinn;
+	for (int i = 0; in; i++){
+		cina[i].aa[i].b;
+	}
+	int t;
+	cint;
+	cintemp.atemp.b;
+	sort(a, a + n, cmp);
+	temp.b -= t;
+	if (temp.b0){
+		int x = 0 - temp.b;
+		temp.a -= x60 + 1;
+		temp.b = 60 - x % 60;
+	}
+	for (int i = n - 1; i = 0; i--){
+		if (a[i].atemp.aa[i].a == temp.a&&a[i].b = temp.b){
+			couta[i].a a[i].bendl;
+			break;
+		}
+	}
+	return 0;
+}
